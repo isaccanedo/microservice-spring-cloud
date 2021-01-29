b9820972142146b12a5038aa1c7ee07fa9910f85
isaccanedo <isaccanedo@gmail.com>
2021-01-29 11:00:34 -0300

    :zap: Initial commit

---
+++ src/main/java/com/isac/cloud/ms/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.isac.cloud.ms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableCircuitBreaker
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
